ccbffe87234189f44dc5ff954b4f4f0d11664dd8
Artur Borecki <me@pufereq.pl>
2025-11-23 13:44:59 +0100

    feat(__main__.py): setup `SerialReceiver` and logging

--- src/museum_book_prototype/__main__.py
+++ src/museum_book_prototype/__main__.py
@@ -1,5 +1,34 @@
 # -*- coding: utf-8 -*-
 """Museum Book Prototype package entry point."""
 
+import logging as lg
+import threading
+
+from museum_book_prototype.serial_receiver import SerialReceiver
+
+
+def main() -> None:
+    """Main function to start the Museum Book Prototype application."""
+    lg.basicConfig(
+        level=lg.DEBUG,
+        format=(
+            "%(asctime)s : %(levelname)-8s : %(threadName)s : %(filename)s:"
+            "%(lineno)d : %(name)s :: %(message)s"
+        ),
+    )
+    # TODO: add rotating file handler
+
+    lg.info("hello!")
+
+    serial_receiver = SerialReceiver()
+    receiver_thread = threading.Thread(
+        name="SerialReceiver", target=serial_receiver.run, daemon=True
+    )
+    receiver_thread.start()
+
+    lg.info("Serial receiver thread started.")
+    receiver_thread.join()
+
+
 if __name__ == "__main__":
-    pass
+    main()
